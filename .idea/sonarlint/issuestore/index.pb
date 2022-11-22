f1b52f446e4cf98428a86d613125fdb50fac4550
Marcio Roberto Vasconcelos Jr <marcioj@ciandt.com>
2022-11-22 11:57:46 -0300

    Criacao dos projetos core - course e discovery

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+ç
+]course/src/main/java/academy/devdojo/youtube/course/endpoint/controller/CourseController.java,f/6/f6cae230d2cdc0540c179a2c2c1ad4f1fa491617
+q
+Acore/src/main/java/academy/devdojo/youtube/core/model/Course.java,0/4/040776016e4fdac79677cd4cfe20dd2636d833e4
+y
+Icore/src/main/java/academy/devdojo/youtube/core/model/AbstractEntity.java,d/0/d0a3cd1324e703d7fbb0117f1dbdb46f336ddc34
+<
+core/pom.xml,7/6/76401d80919958a0380f14132adf8b8fc9c3d6ea
---
+++ core/src/main/java/academy/devdojo/youtube/core/model/Course.java
@@ -0,0 +1,24 @@
+package academy.devdojo.youtube.core.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Course implements AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+}
