e9cad69dbce25780e7eb962829dc732917d54947
Marcio Roberto Vasconcelos Jr <marcioj@ciandt.com>
2022-11-23 21:04:18 -0300

    Criacao de gateway e web security

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,9 @@ y
 Icore/src/main/java/academy/devdojo/youtube/core/model/AbstractEntity.java,d/0/d0a3cd1324e703d7fbb0117f1dbdb46f336ddc34
 <
 core/pom.xml,7/6/76401d80919958a0380f14132adf8b8fc9c3d6ea
+€
+Pcore/src/main/java/academy/devdojo/youtube/core/repository/CourseRepository.java,2/5/2589bcbbea5ebbbefa4dbf47f19d4ede5b661330
+„
+Tcourse/src/main/java/academy/devdojo/youtube/course/repository/CourseRepository.java,c/9/c97bd86702e9f0c4d08a0b1ff58a1c51da1fda7a
+‡
+Wcourse/src/main/java/academy/devdojo/youtube/course/endpoint/service/CourseService.java,e/a/ea5e57e9179001a2aa5917d6debaf953c3f4aee7
---
+++ core/src/main/java/academy/devdojo/youtube/core/model/ApplicationUser.java
@@ -0,0 +1,36 @@
+package academy.devdojo.youtube.core.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class ApplicationUser implements AbstractEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role = "USER";
+
+    public ApplicationUser(ApplicationUser applicationUser) {
+        this.id = applicationUser.getId();
+        this.username = applicationUser.getUsername();
+        this.password = applicationUser.getPassword();
+        this.role = applicationUser.getRole();
+    }
+}
